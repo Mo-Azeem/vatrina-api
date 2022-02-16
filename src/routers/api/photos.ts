06cfa9fc5365513dcfcdd47869f5c417bbe52344
MoeIsLearning <moeislearning@gmail.com>
2022-02-17 00:43:17 +0200

    commit

--- src/routers/api/photos.ts
+++ src/routers/api/photos.ts
@@ -5,34 +5,31 @@ import { jsonfiyPhoto, jsonfiyError } from '../../utils/jsonUtils'
 
 const photos = express.Router()
 
-// Todo: rework this method to get all photos and their thumbs 
-photos.get('/', async (req, res) => {
-    const allPhotos = await fm.allPhotos()
-    return res.send(allPhotos)
-})
+// Fetch all Photos
+photos.get('/', async (req, res) => res.send(await fm.allPhotos()))
 
-photos.post('/', async (req, res) => {
-    try {
-        if (!req.files) {
-            res.send({
-                status: false,
-                message: 'No File was Uploaded'
-            })
-        } else {
-            const result = await fm.savePhoto(req.files.photo as UploadedFile)
-            res.send(result)
-        }
-    } catch (err) {
-        res.status(500).send(err)
-    }
-})
-
+// Fetch One Single Photo Using The File's Name
 photos.get('/:fileName', async (req, res) => {
     if (!fm.doesExist(req.params.fileName))
         return res.status(404).send(jsonfiyError(req.params.fileName))
     res.send(await jsonfiyPhoto(req.params.fileName))
 })
 
+// Upload a Photo and Create a Thumbnail for it, then return a JSON with Photo's and Thumb's Links
+photos.post('/', async (req, res) => {
+    try {
+        if (!req.files) {
+            res.status(400).send({
+                status: false,
+                message: 'No File was Uploaded'
+            })
+        } else {
+            res.send(await fm.savePhoto(req.files.photo as UploadedFile))
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
 
 
 export default photos
--- src/routers/api/thumbnails.ts
+++ src/routers/api/thumbnails.ts
@@ -1,8 +1,5 @@
 import express from 'express'
-import { UploadedFile } from 'express-fileupload'
 import fm from '../../utils/file-manager'
-import resizer from '../../utils/resizer'
-import { jsonfiyPhoto, jsonfiyThumbnail } from '../../utils/jsonUtils'
 
 const thumbnails = express.Router()
 
